8cab870f57b7b4c90862ca6315f3e81be73173d9
pedromdcr <pedromdcrodrigues@gmail.com>
2016-03-21 19:29:16 +0000

    PETRÓILO REFINADO!

--- src/ist/meic/pa/MyProfiler.java
+++ src/ist/meic/pa/MyProfiler.java
@@ -4,8 +4,11 @@ import javassist.bytecode.BadBytecode;
 import javassist.bytecode.analysis.ControlFlow;
 import javassist.bytecode.analysis.ControlFlow.Block;
 import javassist.bytecode.analysis.FramePrinter;
+
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.lang.Throwable;
 import java.util.Arrays;
 public class MyProfiler implements  Translator {
@@ -29,13 +32,35 @@ public class MyProfiler implements  Translator {
 		if (!ctClass.getName().equals("ist.meic.pa.BoxingPorfiler")
 				&& !ctClass.getName().contains("javassist")) {
 			for(CtMethod method : ctClass.getDeclaredMethods()){
-				//System.out.println("vinho verde");
-				PrintStream ps = new PrintStream(System.out, true);
+				
+				/*PrintStream ps = new PrintStream(System.out, true);
 				FramePrinter fp = new FramePrinter(ps);
 				fp.print(method);
-				
+				System.out.println(method.getLongName() + " boxed " + printCounter(method) + " " + ;*/
+				printCounter(method);
 			}
 		}
 		
+	}
+
+	private String printCounter(CtMethod method) {
+		// TODO Auto-generated method stub
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		PrintStream ps = new PrintStream(baos, true);
+		FramePrinter fp = new FramePrinter(ps);
+		fp.print(method);
+		String s = new String();
+		try {
+			s = new String(baos.toByteArray(),"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("I'm string s:");
+		System.out.println(s);
+		
+		return null;
 	}
 }
