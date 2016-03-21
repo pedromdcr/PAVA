49c2971f358d18929ee5c2a3f33c506dc16a09c6
pedromdcr <pedromdcrodrigues@gmail.com>
2016-03-21 20:05:47 +0000

    comentarios

--- src/ist/meic/pa/MyProfiler.java
+++ src/ist/meic/pa/MyProfiler.java
@@ -5,9 +5,11 @@ import javassist.bytecode.analysis.ControlFlow;
 import javassist.bytecode.analysis.ControlFlow.Block;
 import javassist.bytecode.analysis.FramePrinter;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.lang.Throwable;
 import java.util.Arrays;
@@ -44,13 +46,16 @@ public class MyProfiler implements  Translator {
 	}
 
 	private String printCounter(CtMethod method) {
-		// TODO Auto-generated method stub
 		
+		//Creates the necessary Streams and Printers to read the ctMethod info
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		
 		PrintStream ps = new PrintStream(baos, true);
 		FramePrinter fp = new FramePrinter(ps);
+		
+		//prints the ctMethod information into the ByteArrayOutputStream baos
 		fp.print(method);
+		
+		//converts the OutputStream into a String for easy parsing
 		String s = new String();
 		try {
 			s = new String(baos.toByteArray(),"UTF-8");
@@ -58,8 +63,15 @@ public class MyProfiler implements  Translator {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		System.out.println("I'm string s:");
-		System.out.println(s);
+		
+		//reads the string containing the method stack information into a buffered reader
+		BufferedReader bufReader = new BufferedReader(new StringReader(s));
+		
+		//TODO parses the string to locate boxing and unboxing operations
+		
+		//TODO saves counts to a hash map or hash table to print as expected in main
 		
 		return null;
 	}
