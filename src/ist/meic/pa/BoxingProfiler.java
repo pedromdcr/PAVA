4dbb611b4061780516444cd9b2e1480f664d90b4
tiagorodriguessimoes <tiago.rodrigues.simoes@gmail.com>
2016-03-26 19:23:53 +0000

    tarde de sabado

--- src/ist/meic/pa/BoxingProfiler.java
+++ src/ist/meic/pa/BoxingProfiler.java
@@ -1,138 +1,23 @@
 package ist.meic.pa;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import javassist.CannotCompileException;
 import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtField;
-import javassist.CtMethod;
 import javassist.Loader;
-import javassist.NotFoundException;
-import javassist.bytecode.BadBytecode;
-import javassist.bytecode.CodeAttribute;
-import javassist.bytecode.LineNumberAttribute;
-import javassist.expr.ExprEditor;
-import javassist.expr.MethodCall;
+import javassist.Translator;
 
 public class BoxingProfiler {
 
-	public static String[] getCounterType(String methodName, String className, String lineNumber) {
-
-		//System.out.println("LINENUMBER: " + lineNumber + " CLASSNAME " + className + " METHODNAME " + methodName);
-
-		// counterType is [boxed_or_unboxed, wrapperType] e.g. [boxed,
-		// Integer,Linenumber]
-		String counterType[] = new String[3];
-		// parse classname to get wrapper type
-		String[] tokens = className.split("\\.");
-		if (tokens.length >= 3) {
-
-			// store it as the appropriate counter type
-			if (tokens[2].equals("Integer") || tokens[2].equals("Double") || tokens[2].equals("Float")
-					|| tokens[2].equals("Short") || tokens[2].equals("Long")) {
-				counterType[1] = tokens[2];
-				// for(String si : counterType) {
-				// System.out.println("->>>COUNTERTYPE: " + si);
-				// }
-			//	System.out.println("METHOD NAME: " + methodName);
-				if (methodName.equals("valueOf")) {
-					counterType[0] = "boxed";
-				} else if (methodName.contains("Value")) { // falta verificar se
-															// ha mais metodos
-															// que n sejam de
-															// unboxing que
-															// contenham "value"
-					counterType[0] = "unboxed";
-				}
-				counterType[2] = lineNumber;
-				// for(String si : counterType) {
-				// System.out.println("->>>COUNTERTYPE2: " + si);
-				// }
-			}
-		}
-		return counterType;
-	}
-
-	public static void memoize(CtClass ctClass, CtMethod ctMethod, String [] order)
-			throws CannotCompileException, NotFoundException {
-
-		// creates a field to store the counters for autoboxing and unboxing
-		// operations for the specified class
-		if(order[0]==null){return;}
-		
-
-		
-		
-		
-		
-		
-		
-		
-		// writes the changes to the ctClass file
-		try {
-			ctClass.writeFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		ctClass.defrost();
-	}
-
-	public static void main(String[] args) throws NotFoundException, CannotCompileException {
-
-		ClassPool pool = ClassPool.getDefault();
-		final CtClass ctClass = pool.getCtClass(args[0]);
-
-		// final HashMap<String, Integer> counts = new HashMap<String,
-		// Integer>();
-
-		for (final CtMethod method : ctClass.getDeclaredMethods()) {
-			System.out.println("\n\n\n\n\n");
-			System.out.println(method.getName());
-			System.out.println("\n");
-			String countername = "static java.util.TreeMap " + method.getName() + "Count = new java.util.TreeMap();";
-			CtField ctField = CtField.make(countername, ctClass);
-
-			ctClass.addField(ctField);
+	public static void main(String[] args) {
+		try{
+			Translator  translator = new MyProfiler();
+			ClassPool pool = ClassPool.getDefault();
+			Loader classLoader = new Loader();
+			classLoader.addTranslator(pool,translator);
+			String[] restArgs = new String[args.length - 1];
+			System.arraycopy(args, 1, restArgs, 0, restArgs.length);
+			classLoader.run(args[0], restArgs);
+		} catch (Throwable e) {
 			
-
-			//final List<String[]> order = new ArrayList<String[]>();
-
-			method.instrument(new ExprEditor() {
-				public void edit(MethodCall m) throws CannotCompileException {
-					String[] counterType = new String[3]; // counterType[2] is
-															// the line where
-															// the counter is to
-															// be inserted
-
-					try {
-						String name = m.getMethod().getName();
-						if (name.contains("valueOf") || name.contains("Value")) {
-							counterType = getCounterType(m.getMethodName(), m.getClassName(),
-									String.valueOf(m.getLineNumber()));
-							//order.add(0, counterType);
-						}
-					} catch (NotFoundException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-
-					try {
-						memoize(ctClass, method, counterType);
-					} catch (NotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-			});
-
 		}
 
 	}
-
 }
--- src/ist/meic/pa/MyProfiler.java
+++ src/ist/meic/pa/MyProfiler.java
@@ -1,5 +1,8 @@
 package ist.meic.pa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -11,62 +14,110 @@ import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
 public class MyProfiler implements Translator {
+	
+	private CtClass _ctClass;
+	private List<String> names; 
+	
 	public void start(ClassPool pool) 
 			throws NotFoundException, CannotCompileException {
 	}
 
 	public void onLoad(ClassPool pool, String className)
 			throws NotFoundException, CannotCompileException {
-		CtClass ctClass = pool.get(className);
-		make(pool,ctClass);
+		_ctClass = pool.get(className);
+
+		make(pool,_ctClass);
 	}
 
+	
 	void make(ClassPool pool, CtClass ctClass)
 			throws NotFoundException, CannotCompileException {
 		System.out.println("---------\n\tMake\n");
 
 		// tentar enfiar int z para a class..
+//
+//		CtField f = CtField.make("", ctClass);
+//		ctClass.addField(f);
 
-		CtField f = new CtField(CtClass.intType, "z", ctClass);
-		ctClass.addField(f, "0");
+		//
+		//		
 
-	
+		//		CtField f = CtField.make("TreeMap<Integer, String> tmap = new TreeMap<Integer, String>();", ctClass);
+		//		ctClass.addField(f);
 		//ver metodos
+//		CtField hashtableField = CtField.make("public static java.util.TreeMap treeMap = new java.util.TreeMap();", ctClass);
+//		ctClass.addField(hashtableField);
 		for (CtMethod ctMethod : ctClass.getDeclaredMethods()) {
+			names = new ArrayList<String>();
+			System.out.println("------Name:\t"+ctMethod.getName());
+			final String prefix = ctMethod.getName(); // para distinguir metodos com o mesmo nome e assinaturas diferentes
 			ctMethod.instrument(new ExprEditor() {
-				public void edit(MethodCall m)
-						throws CannotCompileException {
-
-
-					if(m.getClassName().equals("java.lang.Integer")||
-							m.getClassName().equals("java.lang.Long")||
-							m.getClassName().equals("java.lang.Short")||
-							m.getClassName().equals("java.lang.Byte")){
-						if(m.getMethodName().equals("valueOf")){
-							System.out.println("Class Name:\t"+m.getClassName());
-							System.out.println("Method Name:\t"+m.getMethodName());
-							System.out.println("Line Number:\t"+m.getLineNumber());
-							System.out.println("");
-
-						}
+				public void edit(MethodCall m) throws CannotCompileException {
+					System.out.println("Class Name:\t"+ m.getClassName());
+					System.out.println("Method Name:\t"+ m.getMethodName());
+					System.out.println("Line Number:\t"+ m.getLineNumber());
+					String[] tokens = m.getClassName().split("\\."); 
+					if(tokens.length == 3 && (m.getClassName().contains("java.lang.") || m.getMethodName().contains("alue"))) {
+						String name = prefix + "_" + tokens[2] + "_" + m.getMethodName();
+						names.add(name);
+						
+						System.out.println(">>>>>>NAME: " + name);
+						
+						
+						//cria um contador não final (ou seja, que pode ser incrementado) com o nome especificado
+						createField(name);
+						
+						m.replace("{ $_ = $proceed($$); " + name + "++; }");
+						
 					}
-				
-
-
-
+					System.out.println("");
+					
 				}
 
 			});
+			
+//			for (String s : names) {
+//				CtField f = CtField.make("public static final int " + s + "_final = " + s + ";", _ctClass);
+//				_ctClass.addField(f);
+//			}
+			
+			//processFinalFields();
 		}
 
 		//ver a variavel z introduzida acima
 		System.out.println("PRINT FIELDS");
 		for (CtField ctm : ctClass.getFields()) {
 			System.out.println(ctm.getName());
-			System.out.println(ctm.getFieldInfo().getName());
+			//System.out.println(ctm.getFieldInfo().getName());
+			//System.out.println(ctm.getConstantValue());
 		}
 
 
 	}
 
+	private void processFinalFields() {
+		
+		for (CtField ctm : _ctClass.getFields()) {
+			if(ctm.getName().contains("_final")) {
+				System.out.println("----->COUNTER NAME: " + ctm.getName() + " = " + ctm.getConstantValue());
+			}
+		}
+		
+	}
+
+	protected void createField(String name) throws CannotCompileException {
+		
+		for (CtField ctm : _ctClass.getFields()) {
+			if(ctm.getName().equals(name)) {
+				return;
+			}
+		}
+		
+		CtField f = CtField.make("public int " + name + "= 0;", _ctClass);
+		_ctClass.addField(f);
+		
+	}
+	
 }
+		
+
